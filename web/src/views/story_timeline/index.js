1f04bd68cff5e46c659b45600156c727506c0de2
kevinzhow <kevinchou.c@gmail.com>
2016-08-07 17:55:44 +0800

    Pass bubbles to data

--- web/src/views/story_timeline/index.js
+++ web/src/views/story_timeline/index.js
@@ -5,15 +5,19 @@ var Component = Vue.extend({
   template: require('./template.html'),
   replace: true,
   created () {
-    store.fetchStory("story_id").then(function(json) {
+    store.fetchStory("story_id").then(json => {
       console.log('parsed story json', json)
+      this.bubbles = [
+        { user: { username:"左耳朵耗子", avatar: "http://tva3.sinaimg.cn/crop.27.27.337.337.180/538efefbgw1eg77da7jggj20aw0aw743.jpg" }, content: "小方这年刚刚考上北邮，正准备投身到祖国四化的伟大事业中，没想到一个人的发是无法预料的，要看历史的进程，小方的爸爸给小方取了个伟大的名字——滨兴，寓意着万邦来朝，祖国可以兴盛伟大。", likes: 130, created_at: "1 分钟前" },
+        { user: { username:"周楷雯Kevin", avatar: "http://tva2.sinaimg.cn/crop.0.0.1242.1242.180/68c9c44djw8f0y66adyekj20yi0yigmt.jpg" }, content: "2017 年，北京政府实行了全面断网的政策，一时间，中国失去了和外界的联系，无数人都在猜测，这是新一代的闭关锁国，还是有所策划的一场大阴谋", likes: 130, created_at: "1 分钟前"}
+      ]
     })
   },
   events: {
-    'timeline-toggleTips': function () {
+    'timeline-toggleTips': () => {
       this.toggleTips()
     },
-    'timeline-toggleComposeDialog': function() {
+    'timeline-toggleComposeDialog': () => {
       this.toggleCompose()
     }
   },
@@ -31,10 +35,7 @@ var Component = Vue.extend({
       tipsDialogState: false,
       composeDialogState: false,
       composeContent: "",
-      bubbles: [
-        { user: { username:"左耳朵耗子", avatar: "http://tva3.sinaimg.cn/crop.27.27.337.337.180/538efefbgw1eg77da7jggj20aw0aw743.jpg" }, content: "小方这年刚刚考上北邮，正准备投身到祖国四化的伟大事业中，没想到一个人的发是无法预料的，要看历史的进程，小方的爸爸给小方取了个伟大的名字——滨兴，寓意着万邦来朝，祖国可以兴盛伟大。", likes: 130, created_at: "1 分钟前" },
-        { user: { username:"周楷雯Kevin", avatar: "http://tva2.sinaimg.cn/crop.0.0.1242.1242.180/68c9c44djw8f0y66adyekj20yi0yigmt.jpg" }, content: "2017 年，北京政府实行了全面断网的政策，一时间，中国失去了和外界的联系，无数人都在猜测，这是新一代的闭关锁国，还是有所策划的一场大阴谋", likes: 130, created_at: "1 分钟前" }
-      ],
+      bubbles: [],
       tipsBubble: {
         user: { username:"左耳朵耗子", avatar: "http://tva3.sinaimg.cn/crop.27.27.337.337.180/538efefbgw1eg77da7jggj20aw0aw743.jpg" }, content: "请点击我要续写续写本故事的矛盾"
       },
