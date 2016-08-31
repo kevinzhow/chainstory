94263913f949e6975486cd8c16c3315f99ccc75e
kevin <kevinchou.c@gmail.com>
2016-08-31 17:02:15 +0800

    Better looking when user not login

--- web/src/components/tips_bubble/index.js
+++ web/src/components/tips_bubble/index.js
@@ -5,11 +5,19 @@ import config from "../../config"
 let component = Vue.extend({
     template: require('./template.html'),
     props: ['bubble'],
+    created() {
+    },
     methods: {
         toggleTips: function (event) {
             this.$dispatch('toggleTips', config.TIP.storyLine.name);
         }
-    }
+    },
+	data: () => {
+		return {
+		  styleData: "",
+		  hasCurrentUser: false
+		}
+	},
 })
 
 module.exports = component
